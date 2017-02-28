65387d82f1606991a766d6e04acb045f7d966c46
awu <softengineerwu@gmail.com>
2017-02-28 13:50:43 +0800

    完成hibernate配置。

--- src/com/awu/actions/SearchAction.java
+++ src/com/awu/actions/SearchAction.java
@@ -1,10 +1,42 @@
 package com.awu.actions;
 
+import java.util.ArrayList;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.awu.models.User;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class SearchAction extends ActionSupport {
 	public String execute(){
 		System.out.println("Search");
+		getData();
 		return SUCCESS;
+	}
+	
+	private void getData(){
+		Configuration configuration = new Configuration().configure();
+		SessionFactory factory = configuration.buildSessionFactory();
+		Session session = null;
+		try{
+			session = factory.openSession();
+			session.beginTransaction();
+//			ArrayList<User> result = (ArrayList<User>) session.createQuery("from User").list();
+			session.createQuery("from User").uniqueResult();
+			System.out.println(session.get(User.class,"402895da5a6def38015a6def39d80000").getUsername());
+			
+			session.getTransaction().commit();
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+			session.getTransaction().rollback();
+		}finally {
+			if(session != null){
+				if(session.isOpen())
+					session.close();
+			}
+		}
+
 	}
 }
---
+++ src/com/awu/models/User.java
@@ -0,0 +1,62 @@
+package com.awu.models;
+
+import java.util.Date;
+
+public class User {
+	private String userid;
+	private String username;
+	private String pwd;
+	private String creator;
+	private Date createtime;
+	private String updater;
+	private Date updatetime;
+	private int isenabled;
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getCreator() {
+		return creator;
+	}
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+	public Date getCreatetime() {
+		return createtime;
+	}
+	public void setCreatetime(Date createtime) {
+		this.createtime = createtime;
+	}
+	public String getUpdater() {
+		return updater;
+	}
+	public void setUpdater(String updater) {
+		this.updater = updater;
+	}
+	public Date getUpdatetime() {
+		return updatetime;
+	}
+	public void setUpdatetime(Date updatetime) {
+		this.updatetime = updatetime;
+	}
+	public int getIsenabled() {
+		return isenabled;
+	}
+	public void setIsenabled(int isenabled) {
+		this.isenabled = isenabled;
+	}
+}
